21b5ca053ab4a1b2cda63981430f1d24b2d0678a
Harris Miller <harrismiller08@gmail.com>
2014-11-10 23:45:42 -0700

    subreddit data working
    thanks to emberjs without ember data

--- app/router.js
+++ app/router.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+	location: config.locationType
 });
 
 Router.map(function() {
+	this.resource('subreddit', { path: '/r/:subreddit_id'});
 });
 
 export default Router;
